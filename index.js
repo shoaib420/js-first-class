9474730bfb25fe8b87a6d7a0c67e8a199d3dbd27
Shoaib khan <shoaibkhanu39@gmail.com>
2024-05-13 11:17:40 +0500

    javascript first class helloworld

---
+++ index.js
@@ -0,0 +1,24 @@
+// alert("welcome to my web")
+
+let FirstName = "shaoib";
+// FirstName = "faisal ..... "
+// alert(FirstName)
+
+// variable rules
+
+// legal 
+
+var First_Name = "shoaib soomro ";
+var First_Name1 = " abdullah ";
+var First_Name2 = "faisal  ";
+var First_Name3 = "ali  ";
+var First_Name4 = " haris  ";
+
+console.log(FirstName  + First_Name4 + First_Name1 + First_Name2)
+
+var num  = 9999999 ;
+console.log(typeof(num))
+
+Number
+String
+Boolean
